9fae06d2576181b8c3de61e68af7b8cac37c8b89
wsugiri <wsugiri@gmail.com>
2013-09-01 20:59:29 +0700

    .

--- src/geframe/assets/js/gelang.slide.js
+++ src/geframe/assets/js/gelang.slide.js
@@ -4,18 +4,20 @@
     this.render = function (rootId) {
         var selector = "#" + (rootId || "impress");
         var slides = options.slides || [];
+        var style = ((options.style === undefined) ? "" : " " + options.style);
 
         var html = "";
         $.each(slides, function (idx, val) {
             var title = ((val.title == undefined) ? "" : "<h1>" + val.title + "</h1>");
             var subtitle = ((val.subtitle == undefined) ? "" : "<h3>" + val.subtitle + "</h3>");
+            var clshtml = style + ((val.cls === undefined) ? "" : " " + val.cls);
             var content = ((val.content == undefined) ? "" : "<p>" + val.content + "</p>");
             var contents = "";
             $.each((val.contents || []), function (idx, val) {
                 contents += ((val.content == undefined) ? "" : "<p>" + val.content + "</p>");
             })
 
-            html += "<div id=\"" + val.name + "\" class=\"step slide\" " +
+            html += "<div id=\"" + val.name + "\" class=\"step slide" + clshtml + "\" " +
                     "data-scale=\"" + (val.scale || "1") + "\"" +
 				    "data-x=\"" + (val.x || 0) + "\" data-y=\"" + (val.y || 0) + "\" data-z=\"" + (val.z || 0) + "\" " +
 					"data-rotate-x=\"" + (val.rx || 0) + "\"" + "data-rotate-y=\"" + (val.ry || 0) + "\"" + "data-rotate-z=\"" + (val.rz || 0) + "\" >" +
